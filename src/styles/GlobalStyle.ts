5e3715d24ccb3d95966fe6e0c637ee346967910a
Renato Meireles <renatodepontes@gmail.com>
2023-12-01 03:48:01 -0300

    style: Adjust the app's default colors

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components/native"
 
 export const defaultTheme = {
-  primary: '#272727',
-  secondary: '#fff',
+  primary: '#0a241f',
+  secondary: '#FAFAFA',
   tertiary: '#f0f0f0',
   borderColor: '#858585',
-  buttonColor: '#0749a6',
+  buttonColor: '#1f6b5e',
 }
 
 export const GlobalStyle = styled.View`
   flex: 1;
   background-color: ${defaultTheme.primary};
-  color: #fff;
+  color: ${defaultTheme.secondary};
 `
